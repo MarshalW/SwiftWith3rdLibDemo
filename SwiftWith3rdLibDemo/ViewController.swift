f060883f0f7406064298675cbbe3d8f1d457eede
Marshal <marshal.wu@qq.com>
2014-11-14 12:05:40 +0800

    first commit

---
+++ SwiftWith3rdLibDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SwiftWith3rdLibDemo
+//
+//  Created by Marshal Wu on 14/11/14.
+//  Copyright (c) 2014年 Marshal Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
+
